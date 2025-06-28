7d0095a7b5d6138c7ebd7c110c140bdd5debf31b
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-06-28 17:31:09 +0300

    Put a Temp Data in Model

--- job-board/app/Models/Job.php
+++ job-board/app/Models/Job.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
-    //
+//    use HasFactory;
+    public static function all(): array
+    {
+        return [
+            ['title' => 'Software Engineer', 'Salary' => '$2000'],
+            ['title' => 'AI Engineer', 'Salary' => '$1800']
+        ];
+    }
 }
