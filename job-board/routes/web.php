56071505e238601df8779fb5d67875126f73d4c4
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-06-29 15:01:07 +0300

    Add about & contact Route

--- job-board/routes/web.php
+++ job-board/routes/web.php
@@ -5,5 +5,7 @@ use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index']);
+Route::get('/about', [IndexController::class, 'about']);
+Route::get('/contact', [IndexController::class, 'contact']);
 
 Route::get('/job', [JobController::class, 'index']);
