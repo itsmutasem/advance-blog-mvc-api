e555533701f2e7b93e9f756aaee52555f2db5341
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-06-30 18:04:00 +0300

    Ternary Operator

--- job-board/resources/views/components/layout.blade.php
+++ job-board/resources/views/components/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Job Board - {{ $title ?? "" }}</title>
+    <title>Job Board {{ isset($title) ? " - " . $title : "" }}</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
