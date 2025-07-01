12ecea75f404c1dff3e9dc2b477203ac3f1a3ae8
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-01 15:54:33 +0300

    Active Nav with Ternary Operator

--- job-board/resources/views/components/layout.blade.php
+++ job-board/resources/views/components/layout.blade.php
@@ -30,9 +30,9 @@ This example requires updating your template:
                                 $default = "text-gray-300 hover:bg-gray-700 hover:text-white";
                             @endphp
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <a href="/" class="rounded-md  px-3 py-2 text-sm font-medium {{ $current }}" aria-current="page">Home</a>
-                            <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium {{ $default }}">About</a>
-                            <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium {{ $default }}">Contact Us</a>
+                            <a href="/" class="rounded-md  px-3 py-2 text-sm font-medium {{ request()->is('/') ? $current : $default }}" aria-current="page">Home</a>
+                            <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium {{ request()->is('about') ? $current : $default }}">About</a>
+                            <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium {{ request()->is('contact') ? $current : $default }}">Contact Us</a>
                         </div>
                     </div>
                 </div>
