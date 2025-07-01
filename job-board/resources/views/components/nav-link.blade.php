2c582e4ab763bc97bc5565d56e39d55d14214e01
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-01 16:27:12 +0300

    Fix Active Nav Error

--- job-board/resources/views/components/nav-link.blade.php
+++ job-board/resources/views/components/nav-link.blade.php
@@ -2,6 +2,6 @@
     $current = "bg-gray-900 text-white";
     $default = "text-gray-300 hover:bg-gray-700 hover:text-white";
 @endphp
-<a href="{{ $href }}" class="rounded-md  px-3 py-2 text-sm font-medium {{ request()->is('/') ? $current : $default }}" aria-current="page">
+<a class="rounded-md  px-3 py-2 text-sm font-medium {{ $active ? $current : $default }}" {{ $attributes }}>
     {{ $slot }}
 </a>
