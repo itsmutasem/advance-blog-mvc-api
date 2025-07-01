edba4a593e202ef8e5b6c4e4d6e4c64dd389d3e5
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-01 15:51:22 +0300

    Switch Active Nav Form String to Variable

--- job-board/resources/views/components/layout.blade.php
+++ job-board/resources/views/components/layout.blade.php
@@ -25,10 +25,14 @@ This example requires updating your template:
                     </div>
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
+                            @php
+                                $current = "bg-gray-900 text-white";
+                                $default = "text-gray-300 hover:bg-gray-700 hover:text-white";
+                            @endphp
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                            <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                            <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact Us</a>
+                            <a href="/" class="rounded-md  px-3 py-2 text-sm font-medium {{ $current }}" aria-current="page">Home</a>
+                            <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium {{ $default }}">About</a>
+                            <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium {{ $default }}">Contact Us</a>
                         </div>
                     </div>
                 </div>
