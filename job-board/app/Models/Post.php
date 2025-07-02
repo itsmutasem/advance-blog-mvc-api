9db9425f445a9729a172c61bdbbc0a1bcac27470
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-02 18:21:23 +0300

    Define Fillable & Guarded

--- job-board/app/Models/Post.php
+++ job-board/app/Models/Post.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+//    use HasFactory;
+    protected $fillable = ['title', 'body', 'published']; // fields that can be updated
+    protected $guarded = ['id']; // cannot be updated/assigned (read only)
 }
