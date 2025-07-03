3eb0210eaf128ca997c6a3320d56aefd3df0c7a6
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-03 15:23:45 +0300

    Get All Post Data From DB and Return it to View

--- job-board/app/Http/Controllers/PostController.php
+++ job-board/app/Http/Controllers/PostController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    function index()
+    {
+        // Eloquent ORM -> Get all data
+        $data = Post::all();
+        return view('posts.index', ['posts' => $data]);
+    }
 }
