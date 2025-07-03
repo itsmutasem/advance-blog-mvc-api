191991cb90d09b6c583d99e290aee224d717b588
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-03 15:25:22 +0300

    Add Post - Index - Route

--- job-board/routes/web.php
+++ job-board/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index']);
@@ -9,3 +10,5 @@ Route::get('/about', [IndexController::class, 'about']);
 Route::get('/contact', [IndexController::class, 'contact']);
 
 Route::get('/job', [JobController::class, 'index']);
+
+Route::get('/blog', [PostController::class, 'index']);
