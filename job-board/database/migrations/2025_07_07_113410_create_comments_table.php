09c9e2853e754cff1ac70e109fa335fef54579a4
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-07 15:11:41 +0300

    Rename Comments Table to Comment

--- job-board/database/migrations/2025_07_07_113410_create_comments_table.php
+++ job-board/database/migrations/2025_07_07_113410_create_comments_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('comment', function (Blueprint $table) {
             $table->id();
             $table->string('author');
             $table->string('content');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('comment');
     }
 };
