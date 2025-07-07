ed47809d9acc6d27a414e5cda2b8e6a481206bfb
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-07 15:41:05 +0300

    Show Title Post and make it with link to visit the Post

--- job-board/resources/views/Comment/index.blade.php
+++ job-board/resources/views/Comment/index.blade.php
@@ -3,6 +3,6 @@
     @foreach($comments as $comment)
         <h2 class="text-2xl">{{ $comment->content }}</h2>
         <p>{{ $comment->author }}</p>
-        <p>{{ $comment->post }}</p>
+        <a href="/blog/{{ $comment->post->id }}">{{ $comment->post->title }}</a>
     @endforeach
 </x-layout>
