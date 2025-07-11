667441247f87a99cf176069bfa97ddac6a48ca96
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-11 21:19:43 +0300

    Make Route to Tags

--- job-board/routes/web.php
+++ job-board/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index']);
@@ -16,5 +18,8 @@ Route::get('/blog/create', [PostController::class, 'create']);
 Route::get('/blog/delete', [PostController::class, 'delete']);
 Route::get('/blog/{id}', [PostController::class, 'show']);
 
-Route::get('/comments', [\App\Http\Controllers\CommentController::class, 'index']);
-Route::get('/comments/create', [\App\Http\Controllers\CommentController::class, 'create']);
+Route::get('/comments', [CommentController::class, 'index']);
+Route::get('/comments/create', [CommentController::class, 'create']);
+
+Route::get('/tags', [TagController::class, 'index']);
+Route::get('/tags/create', [TagController::class, 'create']);
