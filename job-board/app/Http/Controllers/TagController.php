ebd8ab8efb812a02f9b127acd7b927185e72ce14
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-11 21:12:46 +0300

    Tag Controller - Index Method

--- job-board/app/Http/Controllers/TagController.php
+++ job-board/app/Http/Controllers/TagController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    //
+    function index()
+    {
+        $data = Tag::all();
+        return view('tag.index', ['tags' => $data, 'pageTitle' => 'Tags']);
+    }
 }
