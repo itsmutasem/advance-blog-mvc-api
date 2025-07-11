80b36744ca8b46c6e6e14c3e2786a585999f7829
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-11 21:17:44 +0300

    View Tags

---
+++ job-board/resources/views/tag/index.blade.php
@@ -0,0 +1,7 @@
+<x-layout :title="$pageTitle">
+    <h2>Tags</h2>
+    @foreach($tags as $tag)
+        <h2 class="text-2xl">{{ $tag->title }}</h2>
+        <p>{{ $tag->body }}</p>
+    @endforeach
+</x-layout>
