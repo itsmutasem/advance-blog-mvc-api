7f390215a44d7c477dd10e783f1e144d1c9fea72
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-12 22:46:37 +0300

    Set to Use UUID id for Posts

--- job-board/app/Models/Post.php
+++ job-board/app/Models/Post.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    use HasUuids;
     protected $primaryKey = 'id';
-    protected $keyType = 'int';
+    protected $keyType = 'string'; // UUID - Universal Unique Identifier
+    protected $incrementing = false;
     protected $table = 'post';
 //    use HasFactory;
     protected $fillable = ['title', 'body', 'author', 'published']; // fields that can be updated
