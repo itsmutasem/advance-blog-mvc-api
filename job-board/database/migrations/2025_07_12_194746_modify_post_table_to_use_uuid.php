4136687341314255a99cd435c8d2d0796676fa1d
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-12 23:19:56 +0300

    and Add Author Column in UP Method

--- job-board/database/migrations/2025_07_12_194746_modify_post_table_to_use_uuid.php
+++ job-board/database/migrations/2025_07_12_194746_modify_post_table_to_use_uuid.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('body');
             $table->boolean('published');
+            $table->string('author')->after('title');
             $table->timestamps();
         });
     }
