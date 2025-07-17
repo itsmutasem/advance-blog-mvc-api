9f23ae3d6909634334f25c8e544bd77efed54d4c
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-18 00:27:37 +0300

    Reorder the View

--- job-board/resources/views/post/index.blade.php
+++ job-board/resources/views/post/index.blade.php
@@ -2,8 +2,8 @@
     <h2>Blog</h2>
     @foreach($posts as $post)
         <h2 class="text-2xl">{{ $post->title }}</h2>
+        <p class="text-1xl">{{ $post->author }}</p>
         <p>{{ $post->body }}</p>
-        <p>{{ $post->author }}</p>
     @endforeach
     {{ $posts->links() }}
 </x-layout>
