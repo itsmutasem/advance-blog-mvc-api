df616f0d491c2cfc976a1d9aee25ff62d86a93d1
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-18 00:24:49 +0300

    Remove UUID form the View and add the Author

--- job-board/resources/views/post/index.blade.php
+++ job-board/resources/views/post/index.blade.php
@@ -1,8 +1,9 @@
 <x-layout :title="$pageTitle">
     <h2>Blog</h2>
     @foreach($posts as $post)
-        <h2 class="text-2xl">{{ $post->id }} {{ $post->title }}</h2>
+        <h2 class="text-2xl">{{ $post->title }}</h2>
         <p>{{ $post->body }}</p>
+        <p>{{ $post->author }}</p>
     @endforeach
     {{ $posts->links() }}
 </x-layout>
