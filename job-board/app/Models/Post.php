f59e582ae003f152f813b9ba7a872c65192b56d3
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-17 22:25:15 +0300

    Create Post Factory

--- job-board/app/Models/Post.php
+++ job-board/app/Models/Post.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    use HasFactory;
     use HasUuids;
     protected $primaryKey = 'id';
     protected $keyType = 'string'; // UUID - Universal Unique Identifier
