3f112330926542bc18daaab8a42051e209b94cea
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-18 19:38:03 +0300

    Call User Seeder in Database Seeder

--- job-board/database/seeders/DatabaseSeeder.php
+++ job-board/database/seeders/DatabaseSeeder.php
@@ -15,11 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@admin.com',
-            'password' => Hash::make('123')
-        ]);
+        $this->call(UserSeeder::class);
     }
 }
