38dd5b7443f9995a41748e5df83a6ad82497812c
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-18 21:24:03 +0300

    Define UUID Primary Key in Tag Model

--- job-board/app/Models/Tag.php
+++ job-board/app/Models/Tag.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+    // Primary Key
+    protected $primaryKey = 'id';
+    protected $keyType = 'string'; // UUID - Universal Unique Identifier
+    public $incrementing = false;
+
     protected $table = 'tag';
     protected $fillable = ['title'];
     protected $guarded = ['id'];
