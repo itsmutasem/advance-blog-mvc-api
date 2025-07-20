6d23262085a9729c776d38b063f185d9ebdb16a8
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-21 00:05:06 +0300

    Set Create and Delete Post API Routes

---
+++ job-board/routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/blog', [PostController::class, 'create']);
+Route::delete('/blog', [PostController::class, 'delete']);
