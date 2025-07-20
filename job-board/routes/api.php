535a3620c50c64859ba9cb6ca583beb36fb4f1a5
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-21 00:14:20 +0300

    Define API Routes in bootstrap/app.php

--- job-board/routes/api.php
+++ job-board/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::post('/blog', [PostController::class, 'create']);
 Route::delete('/blog', [PostController::class, 'delete']);
 
