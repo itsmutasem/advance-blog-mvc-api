399360f9932f57a29750501cafbb405e7a97b55a
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-21 00:08:24 +0300

    Set Create Tag API Route

--- job-board/routes/api.php
+++ job-board/routes/api.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/blog', [PostController::class, 'create']);
 Route::delete('/blog', [PostController::class, 'delete']);
 
 Route::post('/comment', [CommentController::class, 'create']);
+
+Route::post('/tag', [TagController::class, 'create']);
