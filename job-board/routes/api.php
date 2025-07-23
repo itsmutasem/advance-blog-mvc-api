2f4f9afeb33bb608cce82289463a837b795a38ab
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-24 02:25:14 +0300

    refactor: Change Route Name to post

--- job-board/routes/api.php
+++ job-board/routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\api\PostApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('blog', PostApiController::class);
+Route::apiResource('post', PostApiController::class);
