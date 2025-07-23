f37aa9176c1bbeb05129e0ed91ac77ad07996d07
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-23 21:05:01 +0300

    Set Create Tag Controller and View

--- job-board/resources/views/Comment/create.blade.php
+++ job-board/resources/views/Comment/create.blade.php
@@ -1,3 +1,3 @@
 <x-layout :title="$pageTitle">
-    <h2>Create New Post</h2>
+    <h2>Create New Comment</h2>
 </x-layout>
--- job-board/resources/views/tag/create.blade.php
+++ job-board/resources/views/tag/create.blade.php
@@ -1,1 +1,3 @@
-<?php
+<x-layout :title="$pageTitle">
+    <h2>Create New Tag</h2>
+</x-layout>
