74ce5f4dc45e305f83bcf9ca52920067dfabb2d1
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-24 02:35:20 +0300

    feat: index PostApiController, Get Data and Paginate then Return Response

--- job-board/app/Http/Controllers/api/PostApiController.php
+++ job-board/app/Http/Controllers/api/PostApiController.php
@@ -3,45 +3,32 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostApiController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $data = Post::paginate(15);
+        return response($data, 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
