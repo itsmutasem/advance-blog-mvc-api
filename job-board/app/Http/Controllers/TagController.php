24702d7b370aa34a131bcb736ae0a03703608347
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-23 21:02:05 +0300

    Rename Old TagController and Create a New TagController

--- job-board/app/Http/Controllers/TagController.php
+++ job-board/app/Http/Controllers/TagController.php
@@ -2,33 +2,64 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    function index()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        $data = Tag::all();
-        return view('tag.index', ['tags' => $data, 'pageTitle' => 'Tags']);
+        //
     }
 
-    function create()
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
     {
-        Tag::create([
-            'title' => 'CSS'
-        ]);
-        return redirect('/tags');
+        //
     }
 
-    function testManyToMany()
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
     {
-        $tag = Tag::find(2);
-        $tag->posts()->attach([4]);
-        return response()->json([
-            'tag' => $tag->title,
-            'posts' => $tag->posts
-        ]);
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tag $tag)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tag $tag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tag $tag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tag $tag)
+    {
+        //
     }
 }
