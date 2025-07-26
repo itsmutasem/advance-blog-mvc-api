97c43df2e987f28980696b871716f4bcb51cbf1c
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-26 19:42:18 +0300

    refactor: content form

--- job-board/resources/views/post/create.blade.php
+++ job-board/resources/views/post/create.blade.php
@@ -21,11 +21,11 @@
                     </div>
 
                     <div class="col-span-full">
-                        <label for="about" class="block text-sm/6 font-medium text-gray-900">About</label>
+                        <label for="about" class="block text-sm/6 font-medium text-gray-900">Content</label>
                         <div class="mt-2">
                             <textarea id="about" name="about" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
                         </div>
-                        <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences about yourself.</p>
+                        <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences about the article.</p>
                     </div>
 
                     <div class="col-span-full">
