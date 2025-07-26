c91522002c9d97a07da2a8b9a85955d9e9f5370c
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-26 19:40:12 +0300

    refactor: set title and author form

--- job-board/resources/views/post/create.blade.php
+++ job-board/resources/views/post/create.blade.php
@@ -6,13 +6,17 @@
                 <p class="mt-1 text-sm/6 text-gray-600">Use this form to publish a new post to the blog.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="username" class="block text-sm/6 font-medium text-gray-900">Username</label>
+                    <div class="sm:col-span-3">
+                        <label for="first-name" class="block text-sm/6 font-medium text-gray-900">Title</label>
                         <div class="mt-2">
-                            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <div class="shrink-0 text-base text-gray-500 select-none sm:text-sm/6">workcation.com/</div>
-                                <input id="username" type="text" name="username" placeholder="janesmith" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
-                            </div>
+                            <input id="first-name" type="text" name="first-name" autocomplete="given-name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-3">
+                        <label for="last-name" class="block text-sm/6 font-medium text-gray-900">Author</label>
+                        <div class="mt-2">
+                            <input id="last-name" type="text" name="last-name" autocomplete="family-name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                         </div>
                     </div>
 
