99e41bed6a057d8a4e712636fe79bd7fbea39329
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-26 20:13:36 +0300

    feat: add "create" post in the index blog

--- job-board/resources/views/post/index.blade.php
+++ job-board/resources/views/post/index.blade.php
@@ -1,5 +1,7 @@
 <x-layout :title="$pageTitle">
-    <h2>Blog</h2>
+    <div class="mt-6 flex items-center justify-end gap-x-6">
+        <a href="/blog/create" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create</a>
+    </div>
     @foreach($posts as $post)
         <h2 class="text-2xl">{{ $post->title }}</h2>
         <p class="text-1xl">{{ $post->author }}</p>
