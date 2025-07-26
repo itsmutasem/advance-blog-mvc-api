a26866a785d073f7a21e4eb6eace08d05562765f
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-26 20:04:31 +0300

    style: blog nav bar button

--- job-board/resources/views/components/layout.blade.php
+++ job-board/resources/views/components/layout.blade.php
@@ -29,6 +29,7 @@ This example requires updating your template:
                             <x-nav-link href="/" :active="request()->is('/')">Dashboard</x-nav-link>
                             <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
                             <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+                            <x-nav-link href="/blog" :active="request()->is('blog')">Blog</x-nav-link>
                         </div>
                     </div>
                 </div>
