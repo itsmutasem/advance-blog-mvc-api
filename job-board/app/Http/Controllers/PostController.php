7d4835e491f9c92c259ed3c26741fef46072ab54
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-27 22:46:01 +0300

    feat: define rules of the request

--- job-board/app/Http/Controllers/PostController.php
+++ job-board/app/Http/Controllers/PostController.php
@@ -21,9 +21,7 @@ class PostController extends Controller
     public function store(Request $request)
     {
         $validated = $request->validate([
-            'title' => 'required',
-            'author' => 'required',
-            'body' => 'required'
+            
         ], [
             'title.required' => 'Mandatory field',
             'author.required' => 'Mandatory field',
