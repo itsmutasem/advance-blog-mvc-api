cf28e6957da1859f9748d455c353ab3ac9d2549b
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-27 18:42:58 +0300

    refactor: csrf and method & action form

--- job-board/resources/views/post/create.blade.php
+++ job-board/resources/views/post/create.blade.php
@@ -1,5 +1,6 @@
 <x-layout :title="$pageTitle">
-    <form>
+    <form method="POST" action="/blog">
+        @csrf
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
                 <h2 class="text-base/7 font-semibold text-gray-900">Create New Post</h2>
