fde11ed25f761c542983e549930e450cfd8b3f76
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-27 22:18:46 +0300

    fix: field name in error class

--- job-board/resources/views/post/create.blade.php
+++ job-board/resources/views/post/create.blade.php
@@ -20,7 +20,7 @@
                     <div class="sm:col-span-3">
                         <label for="author" class="block text-sm/6 font-medium text-gray-900">Author</label>
                         <div class="mt-2">
-                            <input id="author" type="text" name="author" value="{{ old('author') }}" autocomplete="author" class=" {{ $errors->has('title') ? 'outline-red-500' : 'outline-gray-300' }} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                            <input id="author" type="text" name="author" value="{{ old('author') }}" autocomplete="author" class=" {{ $errors->has('author') ? 'outline-red-500' : 'outline-gray-300' }} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                         </div>
                         @error('author')
                         <span class="text-red-500">{{ $message }}</span>
@@ -30,7 +30,7 @@
                     <div class="col-span-full">
                         <label for="body" class="block text-sm/6 font-medium text-gray-900">Content</label>
                         <div class="mt-2">
-                            <textarea id="body" name="body" rows="3" class=" {{ $errors->has('title') ? 'outline-red-500' : 'outline-gray-300' }} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">{{ old('title') }}</textarea>
+                            <textarea id="body" name="body" rows="3" class=" {{ $errors->has('body') ? 'outline-red-500' : 'outline-gray-300' }} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">{{ old('title') }}</textarea>
                         </div>
                         <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences about the article.</p>
                         @error('body')
