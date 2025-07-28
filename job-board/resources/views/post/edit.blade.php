04f992922aa44b01ea005baee38a11599f3c3c5b
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-28 23:05:09 +0300

    feat: keep old data in the "edit" post form

--- job-board/resources/views/post/edit.blade.php
+++ job-board/resources/views/post/edit.blade.php
@@ -11,7 +11,7 @@
                     <div class="sm:col-span-3">
                         <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
                         <div class="mt-2">
-                            <input id="title" type="text" name="title" value="{{ $post->title }}" autocomplete="title" class=" {{ $errors->has('title') ? 'outline-red-500' : 'outline-gray-300' }} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                            <input id="title" type="text" name="title" value="{{ old('title', $post->title) }}" autocomplete="title" class=" {{ $errors->has('title') ? 'outline-red-500' : 'outline-gray-300' }} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                         </div>
                         @error('title')
                         <span class="text-red-500">{{ $message }}</span>
@@ -21,7 +21,7 @@
                     <div class="sm:col-span-3">
                         <label for="author" class="block text-sm/6 font-medium text-gray-900">Author</label>
                         <div class="mt-2">
-                            <input id="author" type="text" name="author" value="{{ $post->author }}" autocomplete="author" class=" {{ $errors->has('author') ? 'outline-red-500' : 'outline-gray-300' }} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                            <input id="author" type="text" name="author" value="{{ old('author', $post->author) }}" autocomplete="author" class=" {{ $errors->has('author') ? 'outline-red-500' : 'outline-gray-300' }} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                         </div>
                         @error('author')
                         <span class="text-red-500">{{ $message }}</span>
@@ -32,7 +32,7 @@
                         <label for="body" class="block text-sm/6 font-medium text-gray-900">Content</label>
                         <div class="mt-2">
                             <textarea id="body" name="body" rows="3" class=" {{ $errors->has('body') ? 'outline-red-500' : 'outline-gray-300' }} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                                {{ $post->body }}
+                                {{ old('body', $post->body) }}
                             </textarea>
                         </div>
                         <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences about the article.</p>
@@ -45,7 +45,7 @@
                         <div class="flex gap-3">
                             <div class="flex h-6 shrink-0 items-center">
                                 <div class="group grid size-4 grid-cols-1">
-                                    <input id="published" type="checkbox" name="published" {{ $post->published ? 'checked' : '' }} aria-describedby="published" class="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto" />
+                                    <input id="published" type="checkbox" name="published" {{ old('published') || (!old() && $post->published) ? 'checked' : '' }} aria-describedby="published" class="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto" />
                                     <svg viewBox="0 0 14 14" fill="none" class="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25">
                                         <path d="M3 8L6 11L11 3.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="opacity-0 group-has-checked:opacity-100" />
                                         <path d="M3 7H11" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="opacity-0 group-has-indeterminate:opacity-100" />
