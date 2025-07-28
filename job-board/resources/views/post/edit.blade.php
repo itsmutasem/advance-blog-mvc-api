a2fc7da02583a935c2d378dc685f6b58c5d2c31f
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-28 23:20:15 +0300

    fix: unique title error when edit the post

--- job-board/resources/views/post/edit.blade.php
+++ job-board/resources/views/post/edit.blade.php
@@ -2,6 +2,7 @@
     <form method="POST" action="/blog/{{ $post->id }}">
         @csrf
         @method('PUT')
+        <input type="hidden" name="id" value="{{ $post->id }}" />
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
                 <h2 class="text-base/7 font-semibold text-gray-900">Edit Post: {{ $post->title }}</h2>
