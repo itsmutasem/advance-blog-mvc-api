f75544819dc6b1a14247e4795c618ac0f9646caa
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-28 20:48:27 +0300

    feat: add edit & delete links. style: view each post in card

--- job-board/resources/views/post/index.blade.php
+++ job-board/resources/views/post/index.blade.php
@@ -7,9 +7,15 @@
     <div class="mt-6 flex items-center justify-end gap-x-6">
         <a href="/blog/create" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create</a>
     </div>
-    @foreach($posts as $post)
-        <h2 class="text-2xl">{{ $post->title }}</h2>
-        <p class="text-1xl">{{ $post->author }}</p>
-    @endforeach
+        @foreach($posts as $post)
+            <div class="bg-white rounded-lg shadow-md p-6 mb-6">
+                <h2 class="text-2xl">{{ $post->title }}</h2>
+                <p class="text-1xl text-gray-600">{{ $post->author }}</p>
+                <div class="mt-4">
+                    <a href="blog/{{ $post->id }}/edit" class="text-blue-500 hover:text-gray-500">Edit</a>
+                    <button class="text-red-500 hover:text-gray-500 ml-4">Delete</button>
+                </div>
+            </div>
+        @endforeach
     {{ $posts->links() }}
 </x-layout>
