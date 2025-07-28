dc36b4a4c4b5899635a109ae9b6f25b2118f4684
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-28 20:29:37 +0300

    refactor: don't print the "body" of the "post" in "index" page

--- job-board/resources/views/post/index.blade.php
+++ job-board/resources/views/post/index.blade.php
@@ -10,7 +10,6 @@
     @foreach($posts as $post)
         <h2 class="text-2xl">{{ $post->title }}</h2>
         <p class="text-1xl">{{ $post->author }}</p>
-        <p>{{ $post->body }}</p>
     @endforeach
     {{ $posts->links() }}
 </x-layout>
