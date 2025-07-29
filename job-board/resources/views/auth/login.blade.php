169e27da4c13470cbd3081ef73c80816d332d524
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-30 02:36:01 +0300

    feat: "login" form

--- job-board/resources/views/auth/login.blade.php
+++ job-board/resources/views/auth/login.blade.php
@@ -1,3 +1,42 @@
-<x-layout :title="$pageTitle">
-    <h1>Login Page</h1>
-</x-layout>
+<x-layout-simple :title="$pageTitle">
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" class="mx-auto h-10 w-auto" />
+            <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Login</h2>
+        </div>
+
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form action="/login" method="POST" class="space-y-6">
+                @csrf
+
+                <div>
+                    <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+                    <div class="mt-2">
+                        <input id="email" type="email" name="email" value="{{ old('email') }}" required autocomplete="email" class="{{ $errors->has('email') ? 'outline-red-500' : 'outline-gray-300' }} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                    </div>
+                    @error('email')
+                    <span class="text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div>
+                    <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
+                    <div class="mt-2">
+                        <input id="password" type="password" name="password" required autocomplete="current-password" class="{{ $errors->has('password') ? 'outline-red-500' : 'outline-gray-300' }} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                    </div>
+                    @error('password')
+                    <span class="text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div>
+                    <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        Login
+                    </button>
+                </div>
+            </form>
+
+        </div>
+    </div>
+
+</x-layout-simple>
