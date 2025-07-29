fbce10ac7bc2631ad8ae23f0ec674f6aafedec1a
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-07-30 00:33:10 +0300

    feat: define auth functions in auth controllers and views

--- job-board/app/Http/Controllers/AuthController.php
+++ job-board/app/Http/Controllers/AuthController.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    // @TODO: signup (GET), signup (POST), login (GET), login (POST), logout(POST)
+
+    public function showSignupForm()
+    {
+        return view('auth.signup');
+    }
+
+    public function signup()
+    {
+
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
 }
