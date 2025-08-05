1f0b9a87c5d3c05812d841b1084926b671b05133
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-05 04:09:46 +0300

    feat: login api route

--- job-board/routes/api.php
+++ job-board/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\api\v1\AuthApiController;
 use App\Http\Controllers\api\v1\PostApiController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
         Route::apiResource('post', PostApiController::class);
+        Route::prefix('auth')->group(function () {
+            Route::post('login', [AuthApiController::class, 'login']);
+        });
 });
