2a48b4bf6f56af06e229e392595e7052e9c43796
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-05 03:53:35 +0300

    feat: auth api controller - define credentials

--- job-board/app/Http/Controllers/api/v1/AuthApiController.php
+++ job-board/app/Http/Controllers/api/v1/AuthApiController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 
 class AuthApiController extends Controller
 {
-    //
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+    }
 }
