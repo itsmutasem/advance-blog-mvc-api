67bcb7987c40faf08f7f4daccfa40737631b22b8
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-08 17:55:21 +0300

    feat: handle 401 errors for api route

--- job-board/bootstrap/app.php
+++ job-board/bootstrap/app.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Middleware\OnlyMe;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,5 +18,12 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias(['onlyMe'=> OnlyMe::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        // Handle 401 errors for API route
+        $exceptions->render( function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Unauthenticated.'
+                ], 401);
+            }
+        });
     })->create();
