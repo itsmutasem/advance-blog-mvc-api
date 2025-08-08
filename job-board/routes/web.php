2a6e15deefef071f96d6d0f9955016908c0576c2
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-08 19:13:49 +0300

    feat: define middleware route for roles

--- job-board/routes/web.php
+++ job-board/routes/web.php
@@ -28,15 +28,27 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // Protected Routes
 Route::middleware('auth')->group(function () {
-    Route::get('blog', [PostController::class, 'index']);
-    Route::get('blog/create', [PostController::class, 'create']);
-    Route::post('blog', [PostController::class, 'store']);
-    Route::get('blog/{id}', [PostController::class, 'show']);
-    Route::get('blog/{id}/edit', [PostController::class, 'edit']);
-    Route::put('blog/{id}', [PostController::class, 'update']);
-    Route::delete('blog/{id}', [PostController::class, 'destroy']);
-    
-    Route::resource('comments', CommentController::class);
+
+    // Viewer, Editor, Admin
+    Route::middleware('role:viewer,editor,admin')->group(function () {
+        Route::get('blog', [PostController::class, 'index']);
+        Route::get('blog/{id}', [PostController::class, 'show']);
+        Route::resource('comments', CommentController::class);
+    });
+
+    // Editor, Admin
+    Route::middleware('role:editor,admin')->group(function () {
+        Route::get('blog/{id}/edit', [PostController::class, 'edit']);
+        Route::put('blog/{id}', [PostController::class, 'update']);
+    });
+
+    // Admin
+    Route::middleware('role:admin')->group(function () {
+        Route::get('blog/create', [PostController::class, 'create']);
+        Route::post('blog', [PostController::class, 'store']);
+        Route::delete('blog/{id}', [PostController::class, 'destroy']);
+    });
+
 });
 
 Route::middleware('onlyMe')->group(function () {
