c3c07fefd1fbb503a86308c8954fab01fecbaf72
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-10 03:44:23 +0300

    style(post/create view): "col-span-full" for "title" input field

--- job-board/resources/views/post/create.blade.php
+++ job-board/resources/views/post/create.blade.php
@@ -7,7 +7,7 @@
                 <p class="mt-1 text-sm/6 text-gray-600">Use this form to publish a new post to the blog.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-3">
+                    <div class="col-span-full">
                         <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
                         <div class="mt-2">
                             <input id="title" type="text" name="title" value="{{ old('title') }}" autocomplete="title" class=" {{ $errors->has('title') ? 'outline-red-500' : 'outline-gray-300' }} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
