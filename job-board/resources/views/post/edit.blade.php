0dc75c982cf35189df23eb24b8a7590b6870c7ec
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-10 04:05:14 +0300

    refactor(post/edit view): remove author field

--- job-board/resources/views/post/edit.blade.php
+++ job-board/resources/views/post/edit.blade.php
@@ -9,22 +9,12 @@
                 <p class="mt-1 text-sm/6 text-gray-600">Use this form to update data for the blog.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-3">
+                    <div class="col-span-full">
                         <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
                         <div class="mt-2">
                             <input id="title" type="text" name="title" value="{{ old('title', $post->title) }}" autocomplete="title" class=" {{ $errors->has('title') ? 'outline-red-500' : 'outline-gray-300' }} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                         </div>
                         @error('title')
-                        <span class="text-red-500">{{ $message }}</span>
-                        @enderror
-                    </div>
-
-                    <div class="sm:col-span-3">
-                        <label for="author" class="block text-sm/6 font-medium text-gray-900">Author</label>
-                        <div class="mt-2">
-                            <input id="author" type="text" name="author" value="{{ old('author', $post->author) }}" autocomplete="author" class=" {{ $errors->has('author') ? 'outline-red-500' : 'outline-gray-300' }} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-                        </div>
-                        @error('author')
                         <span class="text-red-500">{{ $message }}</span>
                         @enderror
                     </div>
